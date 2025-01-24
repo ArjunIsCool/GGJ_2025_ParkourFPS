ca86b0f3e00211b6aa234ddc9aa2496798c7847e
Arjun Mehta <39307836+ArjunIsCool@users.noreply.github.com>
2025-01-24 21:29:14 +0530

    stffuf s

--- Assets/Scripts/ParkourController.cs
+++ Assets/Scripts/ParkourController.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class ParkourController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float runSpeed, sprintSpeed;
+    public float jumpForce;
+    bool isGrounded;
+    bool isWallRunning;
+
+    Rigidbody rb;
+
+    float horiz, verti;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
+    {   
+        horiz = Input.GetAxisRaw("Horizontal");
+        verti = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
     {
-        
+        if(horiz == 0 && verti == 0) return;
+        rb.velocity = new Vector3(horiz* runSpeed , rb.velocity.y, verti* runSpeed);
+
+    }
+
+    void Jump()
+    {
+        if(!isGrounded) return;
+
+        isGrounded = false;
+        rb.AddForce(transform.up * jumpForce);
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.transform.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other) 
+    {
+        if(other.transform.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
